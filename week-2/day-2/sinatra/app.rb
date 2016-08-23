9971756313b3c833d417646c035421138dcb1cae
Matt Warren <mwarren@harding.edu>
2016-08-23 14:44:03 -0400

    create sinatra app.

--- week-2/day-2/sinatra/app.rb
+++ week-2/day-2/sinatra/app.rb
@@ -7,7 +7,6 @@ end
 
 
 get "/company" do 
-	"We are looking for investors.<a href='/'>Go back.</a>"
 
 	erb :company
 end
@@ -19,3 +18,51 @@ get "/about" do
 #			|
 	erb :about
 end
+
+get "/pizza" do 
+	@pizza_ingredients = ["Cheese", "Dough", "Tomato Sauce", "Mushrooms", "Pepperoni", "Pineapple" ]
+
+	erb :favorite_pizza
+end
+
+
+users = [
+	{ :username => "starwars", 		:name => "Star Wars", 	:bio => "The official home of Star Wars on Twitter.", 			:avatar => "/starwars.jpg" },
+	{ :username => "celinedion", 	:name => "Celine Dion", :bio => "Posts signed / Publications signées TC = Team Céline", :avatar => "/celinedion.jpg" },
+	{ :username => "webdevmattw", 	:name => "Matt Warren", :bio => "Visit mattwarren.us", 									:avatar => "/mattwarren.jpg" }
+
+]
+
+
+
+
+# http://localhost:4567/users/starwars
+# => 							|
+# => 			-----------------
+# => 			|
+get "/users/:username" do
+				#|
+# => 			-------------------
+# => 							 |
+	@user_name_string = params[:username]
+# => 		|
+# => 		-----------------------------------------------------------
+# => 																  |
+
+	@the_user = users.find { |the_user| the_user[:username] == @user_name_string }
+	if @the_user == nil
+		erb :no_user
+	else
+		erb :user_profile
+	end
+end
+
+
+
+
+
+
+
+
+
+
