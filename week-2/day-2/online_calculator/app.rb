73ad80a8c3717593301295446fb987a9d69f4780
Matt Warren <mwarren@harding.edu>
2016-08-24 10:13:04 -0400

    make one page for operations.

--- week-2/day-2/online_calculator/app.rb
+++ week-2/day-2/online_calculator/app.rb
@@ -10,55 +10,20 @@ get "/" do
 erb :home
 end
 
-######################## ADD ########################
-######################## ADD ########################
-######################## ADD ########################
+post "/calculate" do
 
-get "/add" do
-  erb :add
-end
-
-post "/calculate_add" do
-  @result = calculator.add(params[:first_number].to_f, params[:second_number].to_f).to_s
-  erb :calculate_add
+  if params[:operation] == "add"
+	  @result = calculator.add(params[:first_number].to_f, params[:second_number].to_f).to_s
+  elsif params[:operation] == "subtract"
+  	  @result = calculator.subtract(params[:first_number].to_f, params[:second_number].to_f).to_s		
+  elsif params[:operation] == "multiply"
+  	  @result = calculator.multiply(params[:first_number].to_f, params[:second_number].to_f).to_s	 	  
+  elsif params[:operation] == "divide"
+  	  @result = calculator.divide(params[:first_number].to_f, params[:second_number].to_f).to_s
+  erb :calculate
 
   # File.open("items.json","w") do |f|
   # f.write(items_array.to_json)
 end
 end
 
-######################## SUBTRACT ########################
-######################## SUBTRACT ########################
-######################## SUBTRACT ########################
-get "/subtract" do
-  erb :subtract
-end
-
-post "/calculate_subtract" do
-  @result = calculator.subtract(params[:first_number].to_f, params[:second_number].to_f)
-  erb :calculate_subtract
-end
-
-######################## MULTIPLY ########################
-######################## MULTIPLY ########################
-######################## MULTIPLY ########################
-get "/multiply" do
-  erb :multiply
-end
-
-post "/calculate_multiply" do
-  @result = calculator.multiply(params[:first_number].to_f, params[:second_number].to_f)
-  erb :calculate_multiply
-end
-
-######################## DIVIDE ########################
-######################## DIVIDE ########################
-######################## DIVIDE ########################
-get "/divide" do
-  erb :divide
-end
-
-post "/calculate_divide" do
-  @result = calculator.divide(params[:first_number].to_f, params[:second_number].to_f)
-  erb :calculate_divide
-end
