a03788f6fb025bc19d98f65c5746e235d830320b
Matt Warren <mwarren@harding.edu>
2016-08-24 12:45:10 -0400

    found elsif bug. simplify calculator

--- week-2/day-2/online_calculator/app.rb
+++ week-2/day-2/online_calculator/app.rb
@@ -11,19 +11,23 @@ erb :home
 end
 
 post "/calculate" do
+  x = params[:first_number].to_f
+  y = params[:second_number].to_f
 
   if params[:operation] == "add"
-	  @result = calculator.add(params[:first_number].to_f, params[:second_number].to_f).to_s
+	  @result = calculator.add(x, y).to_s
   elsif params[:operation] == "subtract"
-  	  @result = calculator.subtract(params[:first_number].to_f, params[:second_number].to_f).to_s		
+  	  @result = calculator.subtract(x, y).to_s		
   elsif params[:operation] == "multiply"
-  	  @result = calculator.multiply(params[:first_number].to_f, params[:second_number].to_f).to_s	 	  
+  	  @result = calculator.multiply(x, y).to_s	 	  
   elsif params[:operation] == "divide"
-  	  @result = calculator.divide(params[:first_number].to_f, params[:second_number].to_f).to_s
+  	  @result = calculator.divide(x, y).to_s
+  end
+
   erb :calculate
 
   # File.open("items.json","w") do |f|
   # f.write(items_array.to_json)
-end
+
 end
 
