3b9346505d8b3093ee54ee19cbbb3886df411a44
Matt Warren <mwarren@harding.edu>
2016-08-24 16:31:40 -0400

    add todo_list to task_index.erb; view shows each task

--- week-2/day-1/sinatra_todo/app.rb
+++ week-2/day-1/sinatra_todo/app.rb
@@ -1,2 +1,25 @@
+#Inside of app.rb
+require 'sinatra'
+require 'sinatra/reloader'
+
+# We're going to need to require our class files
 require_relative("lib/task.rb")
 require_relative("lib/todo_list.rb")
+
+walk = Task.new("Walk the dog")
+feed = Task.new("Feed the pig")
+rob = Task.new("Rob the bank")
+help = Task.new("Help the old lady cross the street")
+
+todo_list = TodoList.new
+todo_list.add_task(walk)
+todo_list.add_task(feed)
+todo_list.add_task(rob)
+todo_list.add_task(help)
+
+
+get "/" do
+	@todo_list = todo_list	
+
+	erb :task_index
+end
