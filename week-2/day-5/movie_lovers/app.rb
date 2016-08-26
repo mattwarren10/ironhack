4ccb70298db58f588e27dfc9e456225c6bf0fd77
Matt Warren <mwarren@harding.edu>
2016-08-26 11:18:55 -0400

    return 9 movies from imdb.

--- week-2/day-5/movie_lovers/app.rb
+++ week-2/day-5/movie_lovers/app.rb
@@ -2,9 +2,15 @@ require "sinatra"
 require "sinatra/reloader" if development?
 require "imdb"
 
+get "/" do
+
+
+	erb :home
+end
+
 get "/search_results" do
 
-	the_search = Imdb::Search.new("alien")
+	the_search = Imdb::Search.new(params[:search_term])
 
 	@movies = the_search.movies
 
