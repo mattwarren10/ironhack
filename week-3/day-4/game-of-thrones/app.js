bcda706e6f9b283a4202d4f915848d016dabae64
Matt Warren <mwarren@harding.edu>
2016-09-01 14:55:10 -0400

    keep episodes greater than 8.5

--- week-3/day-4/game-of-thrones/app.js
+++ week-3/day-4/game-of-thrones/app.js
@@ -4,9 +4,23 @@ function fileActions(err, file){
         throw err;
     }
     var episodes = JSON.parse(file);
-    console.log(`Title: ${episodes[0].title} Episode #: ${episodes[0].episode_number}`);
-    console.log(`${episodes[0].description}`);
-    console.log(`Rating: ${episodes[0].rating}`);
+	var sortedEpisodes = episodes.sort(function(a, b){
+		return a.episode_number - b.episode_number;
+	});
+	var filteredEpisodes = sortedEpisodes.filter(removeShittyEpisodes);
+    for (var i=0; i < filteredEpisodes.length; i++ ) {
+    	console.log(`Title: ${filteredEpisodes[i].title} Episode #: ${filteredEpisodes[i].episode_number}`);
+    	console.log(`${filteredEpisodes[i].description}`);
+    	console.log(`Rating: ${filteredEpisodes[i].rating} ${ratingToStar(filteredEpisodes[i].rating)}`);	
+    }
+    
+}
 
+function ratingToStar (rating) {
+	return  "*".repeat(parseInt(rating));
 }
 fs.readFile("./data/data.json", 'utf8', fileActions);
+
+function removeShittyEpisodes (episode) {
+	return episode.rating > 8.5;
+}
