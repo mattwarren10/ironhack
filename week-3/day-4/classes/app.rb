9815ca51cea8305d363493e1d612ab78653e9616
Matt Warren <mwarren@harding.edu>
2016-09-01 09:28:52 -0400

    ruby: add basic animal class

---
+++ week-3/day-4/classes/app.rb
@@ -0,0 +1,9 @@
+require_relative("lib/animal.rb")
+#app.rb
+
+dymitri = Animal.new("Dymitri", "Huff...")
+dymitri.make_noise
+
+chewie = Animal.new("Chewie", "Woof")
+chewie.make_noise
+chewie.shout
