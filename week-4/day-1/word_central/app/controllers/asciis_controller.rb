a04cd3b788a65352b8149b72a01be20361c912ff
Matt Warren <mwarren@harding.edu>
2016-09-06 18:33:54 -0400

    iteration 4: complete ascii conversion

--- week-4/day-1/word_central/app/controllers/asciis_controller.rb
+++ week-4/day-1/word_central/app/controllers/asciis_controller.rb
@@ -1,2 +1,15 @@
 class AsciisController < ApplicationController
+	def new 
+
+		render 'new'
+	end
+
+	def create
+
+		@text = params[:textarea][:text]
+		@the_ascifier = Artii::Base.new :font => 'slant'
+		@new_word = @the_ascifier.asciify(@text)
+
+		render 'create'
+	end
 end
--- week-4/day-1/word_central/config/routes.rb
+++ week-4/day-1/word_central/config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get '/text_inspections/new', to: 'text_inspections#new'
   post '/text_inspections', to: 'text_inspections#create'
   get '/asciis/new', to: 'asciis#new'
+  post '/asciis', to: 'asciis#create'
 end
