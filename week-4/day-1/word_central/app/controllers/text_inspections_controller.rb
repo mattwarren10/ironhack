86db8888ed6166087fa46b41a1bf7989b1d593c5
Matt Warren <mwarren@harding.edu>
2016-09-06 16:10:09 -0400

    finish iteration 3: add word frequency feature

--- week-4/day-1/word_central/app/controllers/text_inspections_controller.rb
+++ week-4/day-1/word_central/app/controllers/text_inspections_controller.rb
@@ -7,7 +7,13 @@ class TextInspectionsController < ApplicationController
 	def create
 		@text = params[:text_inspection][:user_text]
 		@word_count = @text.split(" ").length
+		@words = @text.split(" ")
 		@minutes = @word_count.to_f / 275 * 60
+		@counts = Hash.new(0)
+		@words.each do |word| 
+			@counts[word] += 1
+		end
+		@sorted_hash = @counts.sort_by{ |k, v| v}.reverse.take(10)
 		render "results"
 	end
 end
