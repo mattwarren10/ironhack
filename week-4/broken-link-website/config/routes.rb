df159dd302336ca89313b6e7a6e43933ced95b47
Matt Warren <mwarren@harding.edu>
2016-09-06 14:50:41 -0400

    mv broken_link_website into day-1

--- week-4/broken-link-website/config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  get '/' => 'visitors#home', as: :home
-  get '/company/about' => 'visitors#about', as: :about
-  get '/blog' => 'visitors#blog', as: :blog
-  get '/contact' => 'visitors#contact', as: :contact
-end
