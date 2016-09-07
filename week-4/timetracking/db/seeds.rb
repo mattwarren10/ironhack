15b56f7d1b6019b31a495717d39921a7c3b933c4
Matt Warren <mwarren@harding.edu>
2016-09-07 15:17:57 -0400

    add data to seeds.rb

--- week-4/timetracking/db/seeds.rb
+++ week-4/timetracking/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Project.create!(name: "Josh's Project", description: "Josh's project description")
+Project.create!(name: "Time Tracking App")
+Project.create!(name: "Recipes", description: "Track my favorite recipes")
+Project.create!(name: "Books", description: "Track my favorite books")
+Project.create!(name: "Movies", description: "Track my favorite movies")
+Project.create!(name: "Bicycles", description: "Track my favorite bicycles")
+Project.create!(name: "Cars", description: "Track my favorite cars")
+Project.create!(name: "Computers", description: "Track my favorite computers")
+Project.create!(name: "Homeperc", description: "Track my favorite homeperc projects")
+Project.create!(name: "Cities", description: "Track my favorite cities")
