79a5556fc999c9a49bee762bde01e4452a640eea
Matt Warren <mwarren@harding.edu>
2016-09-07 16:42:03 -0400

    reorder contact list alphabetically

--- week-4/day-2/address_book/app/controllers/contacts_controller.rb
+++ week-4/day-2/address_book/app/controllers/contacts_controller.rb
@@ -1,11 +1,23 @@
 class ContactsController < ApplicationController
 	def index
-		@contacts = Contact.order(created_at: :desc)
+		@contacts = Contact.order(name: :ASC)
 		render 'index'
 	end
 
 	def new
-		
+
 		render 'new'
+	end
+
+	def create 
+		contact = Contact.new(
+	      :name => params[:contact][:name],
+	      :address => params[:contact][:address],
+	      :phone_number => params[:contact][:phone],
+	      :email => params[:contact][:email])
+
+		contact.save
+
+		redirect_to("/contacts")
 	end
 end
