1f9ca63863398dd6fc318674ec2ec53a488d84c1
Matt Warren <mwarren@harding.edu>
2016-09-07 17:41:42 -0400

    change last name of Frank Adames to Frank Warren

--- week-4/day-2/address_book/db/seeds.rb
+++ week-4/day-2/address_book/db/seeds.rb
@@ -7,6 +7,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Contact.create(name: "Matt Warren", address: "4383 S Leonard Springs Road", phone_number: "812-606-0312", email: "mwarren@harding.edu")
-Contact.create(name: "Frank Adamaes", address: "2458 Fairway Lane", phone_number: "786-521-7138", email: "fadames@gmail.com")
+Contact.create(name: "Frank Warren", address: "2458 Fairway Lane", phone_number: "786-521-7138", email: "fadames@gmail.com")
 Contact.create(name: "Luke Anderson", address: "725 Ulcer Drive", phone_number: "232-383-8329", email: "landerson@gmail.com")
 Contact.create(name: "Paul Howey", address: "342 Rose Avenue", phone_number: "834-234-9847", email: "phowey@gmail.com")
