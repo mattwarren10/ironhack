afa7574c6b4e6841488bfe7be0476814503dd6ed
Matt Warren <mwarren@harding.edu>
2016-09-07 09:11:26 -0400

    iteration 5: Generate placeholder text from quotes

--- week-4/day-1/word_central/app/controllers/placeholder_text_controller.rb
+++ week-4/day-1/word_central/app/controllers/placeholder_text_controller.rb
@@ -5,7 +5,9 @@ class PlaceholderTextController < ApplicationController
 	end
 
 	def create
-		@number = params[:user][:number]
+		@number = params[:user][:number].to_i
 		@select = params[:user][:select]
+		@word = "Lorem " * @number
+		@paragraph = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
 	end
 end
