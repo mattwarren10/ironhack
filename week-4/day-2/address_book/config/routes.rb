fb42cb1ba9da0aee99b1f0f1a4c8d92667d4d948
Matt Warren <mwarren@harding.edu>
2016-09-07 17:04:22 -0400

    contacts#show action

--- week-4/day-2/address_book/config/routes.rb
+++ week-4/day-2/address_book/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/contacts/index', to: 'contacts#index'
+  get '/contacts', to: 'contacts#index'
   get '/contacts/new', to: 'contacts#new'
-
+  post '/contacts', to: 'contacts#create'
+  get 'contacts/show', to: 'contacts#show'
 end
