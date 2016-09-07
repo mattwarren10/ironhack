f9b1ede44301b01cf9ac2a7950a6f2ad263b3fdd
Matt Warren <mwarren@harding.edu>
2016-09-07 09:15:21 -0400

    placeholder_text: allow choosing of multiple paragraphs.

--- week-4/day-1/word_central/app/controllers/placeholder_text_controller.rb
+++ week-4/day-1/word_central/app/controllers/placeholder_text_controller.rb
@@ -8,6 +8,6 @@ class PlaceholderTextController < ApplicationController
 		@number = params[:user][:number].to_i
 		@select = params[:user][:select]
 		@word = "Lorem " * @number
-		@paragraph = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
+		@paragraph = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. " * @number
 	end
 end
