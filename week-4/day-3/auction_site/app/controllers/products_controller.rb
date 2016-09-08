ba75ede487df34e089c14d0f6a6fb98fa4c82b62
Matt Warren <mwarren@harding.edu>
2016-09-08 18:16:55 -0400

    make create action; list all items for one user

--- week-4/day-3/auction_site/app/controllers/products_controller.rb
+++ week-4/day-3/auction_site/app/controllers/products_controller.rb
@@ -16,6 +16,14 @@ class ProductsController < ApplicationController
 	end
 
 	def create
+		user = User.find(params[:user_id])
+		product = user.products.new(
+			title: params[:product][:title],
+			description: params[:product][:description],
+			deadline: params[:product][:deadline]
+		)
+		product.save
+		redirect_to products_path(user)
 	end
 
 	def destroy
--- week-4/day-3/auction_site/config/routes.rb
+++ week-4/day-3/auction_site/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/users/:user_id/products', to: 'products#index'
+  get '/users/:user_id/products', to: 'products#index',as: 'products'
   get '/users/:user_id/products/new', to: 'products#new'
   post '/users/:user_id/products', to: 'products#create', as: 'user_products'
 end
