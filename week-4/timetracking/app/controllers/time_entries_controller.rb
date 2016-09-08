c906fb3063510a8e69ac7c1da7770c399794678c
Matt Warren <mwarren@harding.edu>
2016-09-08 15:47:36 -0400

    make create action; save time_entry; redirect to projects id

--- week-4/timetracking/app/controllers/time_entries_controller.rb
+++ week-4/timetracking/app/controllers/time_entries_controller.rb
@@ -6,6 +6,22 @@ class TimeEntriesController < ApplicationController
 		@time_entries = @project.time_entries.where(
 			date: Time.now.beginning_of_day..Time.now.end_of_day
 		)
+	end
 
+	def new
+		@project = Project.find(params[:project_id])
+		@time_entry = @project.time_entries.new
+	end
+
+	def create
+		project = Project.find(params[:project_id])
+		time_entry = project.time_entries.new(
+			minutes: params[:time_entry][:minutes],
+			hours: params[:time_entry][:hours],
+			date: params[:time_entry][:date]
+		)
+
+		time_entry.save
+		redirect_to '/projects/#{project.id}/time_entries'
 	end
 end
