4671844f7754316f596c192e923386d70f5b99fa
Matt Warren <mwarren@harding.edu>
2016-09-08 16:38:55 -0400

    model project: leave off escape character from format validation

--- week-4/timetracking/app/models/project.rb
+++ week-4/timetracking/app/models/project.rb
@@ -1,3 +1,7 @@
 class Project < ApplicationRecord
 	has_many :time_entries	
+	validates :name, presence: true, 
+					 uniqueness: true, 
+					 length: { maximum: 30 },
+					 format: { with: /[a-zA-Z]/}
 end
