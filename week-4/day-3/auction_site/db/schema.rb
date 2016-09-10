68c9600ab1ac966130373880516cb17bd0909a08
Matt Warren <mwarren@harding.edu>
2016-09-10 16:05:21 -0400

    add form to each product#show

--- week-4/day-3/auction_site/db/schema.rb
+++ week-4/day-3/auction_site/db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160908204926) do
+ActiveRecord::Schema.define(version: 20160910193410) do
+
+  create_table "bids", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "product_id"
+    t.integer  "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_bids_on_product_id"
+    t.index ["user_id"], name: "index_bids_on_user_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.integer  "user_id"
