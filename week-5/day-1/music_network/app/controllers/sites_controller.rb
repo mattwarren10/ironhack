dfed11400f6c9c4ed97b7d4e0afb9ad66e1f2a9b
Matt Warren <mwarren@harding.edu>
2016-09-12 18:02:55 -0400

    interpolate concert.artist on sites#index

--- week-5/day-1/music_network/app/controllers/sites_controller.rb
+++ week-5/day-1/music_network/app/controllers/sites_controller.rb
@@ -1,8 +1,8 @@
 class SitesController < ApplicationController
 	def index
 		@concerts = Concert.all
-		@date_within_one_month = Concert.where("date >= ?", Time.now).where("date <= ?", Time.now.utc.end_of_month)
-		@date_until_today = Concert.where("date >= ?", Time.now).where("date <= ?", Time.now.utc.end_of_day)
+		@date_within_one_month = Concert.where("date >= ?", Time.now).where("date <= ?", Time.now.end_of_month)
+		@date_until_today = Concert.where("date >= ?", Time.now).where("date <= ?", Time.now.end_of_day)
 	end
 
 end
