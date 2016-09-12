980dcdf1764c0e850a1f77fc552872112fea46d1
Matt Warren <mwarren@harding.edu>
2016-09-12 09:50:06 -0400

    add resources to routes

--- week-4/timetracking/config/routes.rb
+++ week-4/timetracking/config/routes.rb
@@ -1,18 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :projects
+
   get '/', to: 'site#home'
 
   get '/contact', to: 'site#contact'
 
   get '/say_name/:name', to: 'site#say_name'
-
-  post '/projects', to: 'projects#create'
-
-  get '/projects', to: 'projects#index'
-
-  get '/projects/new', to: 'projects#new'
-
-  get '/projects/:id', to: 'projects#show'
 
   get '/projects/:project_id/time_entries', to: "time_entries#index"
 
