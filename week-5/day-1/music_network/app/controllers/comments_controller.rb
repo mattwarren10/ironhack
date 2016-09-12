95bd3b2c2195d0328d726d0fdfadbee890d969a7
Matt Warren <mwarren@harding.edu>
2016-09-12 15:44:01 -0400

    add comments to each concert

--- week-5/day-1/music_network/app/controllers/comments_controller.rb
+++ week-5/day-1/music_network/app/controllers/comments_controller.rb
@@ -1,6 +1,22 @@
 class CommentsController < ApplicationController
+	def index
+		@concert = Concert.find(params[:concert_id])
+		@comments = @concert.comments
+	end
 	def new
 		@concert = Concert.find(params[:concert_id])
 		@comment = @concert.comments.new
+	end
+
+	def create
+		concert = Concert.find(params[:concert_id])
+		comment = concert.comments.new(comments_params)
+		comment.save
+		redirect_to concert_comments_path
+	end
+
+	private
+	def comments_params
+		params.require(:comment).permit(:author, :date, :content)
 	end
 end
