e07c2d654bca6eb7a18f6d6cfb70c4817f941c6f
Matt Warren <mwarren@harding.edu>
2016-09-13 13:46:21 -0400

    add styles

--- week-5/jquery/app.js
+++ week-5/jquery/app.js
@@ -1,23 +1,41 @@
 $(document).ready(function() {
 		//page has finished loading.
-	$('.hello-button').on('click', function () {
-	        alert('hello world')
-	    });
+	// $('.hello-button').on('click', function () {
+	//         alert('hello world')
+	//     });
 
-	$('.js-tacos').on('click', function () {
-			alert('eat tacos')
-		});
+	// $('.js-tacos').on('click', function () {
+	// 		alert('eat tacos')
+	// 	});
 
-	var price = '<p>Only $399.99</p>'
-	$('.container').append(price);
+	// var price = '<p>Only $399.99</p>'
+	// $('.container').append(price);
 
-	$('body').append("<h1>Empanadas </h1>");
+	// $('body').append("<h1>Empanadas </h1>");
 
-	$('.container').prepend('<p>Prepend me.</p>');
-	$('.container').before('<p>Before me.</p>');
-	$('.container').after('<p>After me.</p>');
+	// $('.container').prepend('<p>Prepend me.</p>');
+	// $('.container').before('<p>Before me.</p>');
+	// $('.container').after('<p>After me.</p>');
 
-	$('#cookies').remove();
-	$('#exercise').empty();
+	// $('#cookies').remove();
+	// $('#exercise').empty();
 
+	$('.btn-success').on("click", function () {
+		$('.container').toggle()	
+	});
+
+	$('.fader').on("click", function() {
+		if ( $('.container').length > 0 ) {
+			$('.container').fadeOut();
+		}
+		
+		else { 
+			$('.container').fadeIn();
+		}
+
+	});
+
+	$('.unicorn').on("click", function() {
+		$('.hello-button').toggleClass('unicorn-mode');
+	});
 })
