91da96c3ce3aba1f0ac977c6de6fc467003acabd
Matt Warren <mwarren@harding.edu>
2016-09-14 12:24:09 -0400

    add form to display character name, occupation, and weapon

--- week-5/day-3/ajax-practice/app.js
+++ week-5/day-3/ajax-practice/app.js
@@ -1,7 +1,17 @@
 $(document).ready(function(){
-	$(".js-fetch-characters").on("click", fetchCharacters)
+	$(".js-fetch-characters").on("click", fetchCharacters);
+	$(".js-add-obi-wan").on("click", addObiWan);
+	$(".js-add-new-character").on("click", addNewCharacter)
 
 });
+
+function addNewCharacter (theEvent) {
+	theEvent.preventDefault();
+	var name = $(".js-name").val();
+	var occupation = $(".js-occupation").val();
+	var weapon = $(".js-weapon").val();
+	console.log(`Name: ${name}, Occupation: ${occupation}, Weapon: ${weapon}`)
+}
 
 function fetchCharacters () {
 	//AJAX code here
@@ -12,6 +22,7 @@ function fetchCharacters () {
 		error: handleError
 	});
 };
+
 
 function showCharacters (response) {
 	console.log("Success!");
@@ -29,5 +40,26 @@ function showCharacters (response) {
 
 function handleError (error) {
 	console.log("Error!")
-	console.log(error.responseText);
+	console.log(error.statusText);
+}
+
+function addObiWan () {
+	var newCharacter = {
+		name: "Obi Wan Kenobi",
+		occupation: "Jedi Knight",
+		weapon: "Light Sabor"
+	};
+
+	$.ajax({
+		type: "POST",
+		url: "https://ironhack-characters.herokuapp.com/characters",
+		data: newCharacter,
+		success: updateList,
+		error: handleError
+	})
+}
+
+function updateList (response) {
+	console.log("Create Obi Wan Success");
+	console.log(response);
 }
