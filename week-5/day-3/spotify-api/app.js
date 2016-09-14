00196a7f22c68d4c175621f9f117943263b1f91b
Matt Warren <mwarren@harding.edu>
2016-09-14 19:23:05 -0400

    add modal box on 'a' click

--- week-5/day-3/spotify-api/app.js
+++ week-5/day-3/spotify-api/app.js
@@ -1,9 +1,11 @@
 $(document).ready(function (){
-	$(".js-search-spotify").on("click", searchAPI)
+	$(".js-search-spotify-artists").on("click", searchArtists)
+	$("ul").on("click", "a", searchAlbums)
+
 
 });
 
-function searchAPI (e) {
+function searchArtists (e) {
 	e.preventDefault();
 	var value = $(".input-search-spotify").val();
 	$.ajax({
@@ -14,6 +16,17 @@ function searchAPI (e) {
 	})
 }
 
+function searchAlbums(e) {
+	e.preventDefault();
+	$.ajax({
+		type: "GET",
+		url: `https://api.spotify.com/v1/artists/4gzpq5DPGxSnKTe4SA8HAU/albums`,
+		success: showAlbum,
+		error: handleError
+
+	})
+}
+
 function showArtist(response) {
 	console.log("Success!");
 	console.log(response)
@@ -21,16 +34,26 @@ function showArtist(response) {
 	spotifyResults.artists.items.forEach(function(theArtist){
 		var html = `
 			<li>${theArtist.name}</li>
+			<a href="#">${theArtist.name} albums</a>
 		`;
 		theArtist.images.forEach(function (image) {
 			html +=	`<img src="${image.url}">`
 		})
 		$(".js-place-artist").append(html);
 	})
+}
 
+function showAlbum(response) {
+	console.log("Good to go.")
+	console.log(response)
+	$('#myModal').modal('show')
 }
 
 function handleError (error) {
 	console.log("Error");
 	console.log(error.responseText)
 }
+
+
+//album link:
+// https://api.spotify.com/v1/artists/${theArtist.id}/albums
