dce3b83d4c99063d524c4256bfb6f13a92a9b7a1
Matt Warren <mwarren@harding.edu>
2016-09-22 09:15:03 -0400

    sandwichr: add redirects when user is not logged in.

--- week-6/day-1/sandwichr/app/controllers/sandwich_views_controller.rb
+++ week-6/day-1/sandwichr/app/controllers/sandwich_views_controller.rb
@@ -1,10 +1,20 @@
 class SandwichViewsController < ApplicationController
 	def index
 		@sandwiches = Sandwich.all
+		if user_signed_in?
+			render :index
+		else 
+			redirect_to "/"
+		end
 	end
 
 	def show
 		@sandwich = Sandwich.find(params[:id])
 		@ingredients = Ingredient.all
+		if user_signed_in?
+			render :show
+		else 
+			redirect_to "/"
+		end
 	end
 end
